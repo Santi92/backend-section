f8f3e8a3fa50f8145b0b99abe61f361a2fdf9636
Santiago Orozco <santiago.orozco@sundevs.com>
2021-03-29 22:28:14 -0500

    section finish

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const { MONGO_URI } = container.resolve("config");
 
 const mongoose = require("mongoose");
 mongoose
-    .connect(MONGO_URI, { useNewUrlParser : true, useFindAndModify: false})
+    .connect(MONGO_URI, { useNewUrlParser : true, useFindAndModify: false, useUnifiedTopology: true})
     .then(()=> server.start())
     .catch(console.log);
